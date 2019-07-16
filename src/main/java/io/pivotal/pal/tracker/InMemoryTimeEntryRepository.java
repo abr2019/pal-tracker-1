9f51820d4f61132561923cd3f3cf7532bc258afa
rothba <allan.rothberg@emc.com>
2019-07-16 00:05:19 -0400

    spring-mvc: updated file per lab

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,16 +1,16 @@
 package io.pivotal.pal.tracker;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryTimeEntryRepository {
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
 
     private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
 
     private long currentId = 1L;
 
+    @Override
     public TimeEntry create(TimeEntry timeEntry) {
         Long id = currentId++;
 
@@ -27,18 +27,18 @@ public class InMemoryTimeEntryRepository {
 
     }
 
-//    @Override
-    public TimeEntry find(long id) {
+    @Override
+    public TimeEntry find(Long id) {
 
         return timeEntries.get(id);
     }
 
-//    @Override
+    @Override
     public List<TimeEntry> list() {
         return new ArrayList<>(timeEntries.values());
     }
 
-//    @Override
+    @Override
     public TimeEntry update(Long id, TimeEntry timeEntry) {
         if (find(id) == null) return null;
 
@@ -54,7 +54,7 @@ public class InMemoryTimeEntryRepository {
         return updatedEntry;
     }
 
-//    @Override
+    @Override
     public void delete(Long id) {
         timeEntries.remove(id);
     }
--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -3,7 +3,6 @@ package io.pivotal.pal.tracker;
 import java.time.LocalDate;
 
 public class TimeEntry {
-
     private long id;
     private long projectId;
     private long userId;
@@ -52,5 +51,38 @@ public class TimeEntry {
         return hours;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        TimeEntry timeEntry = (TimeEntry) o;
+
+        if (id != timeEntry.id) return false;
+        if (projectId != timeEntry.projectId) return false;
+        if (userId != timeEntry.userId) return false;
+        if (hours != timeEntry.hours) return false;
+        return date != null ? date.equals(timeEntry.date) : timeEntry.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (projectId ^ (projectId >>> 32));
+        result = 31 * result + (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + hours;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeEntry{" +
+                "id=" + id +
+                ", projectId=" + projectId +
+                ", userId=" + userId +
+                ", date='" + date + '\'' +
+                ", hours=" + hours +
+                '}';
+    }
 }
